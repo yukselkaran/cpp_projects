4522b6386c4175b93e485e97f3ee215a5d196d63
Yüksel Karan <ykselkaran53@gmail.com>
2025-07-10 15:12:54 +0300

    BestTimeToBuyAndSellStock.cpp

---
+++ BestTimeToBuyAndSellStock.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+int main() {
+	vector<int> prices = { 12,23,20,87,102,1 };
+	int maxProfit = 0;
+	for (int i = 0; i < prices.size(); i++) {
+		for (int j = i + 1; j < prices.size(); j++) {
+			int profit = prices[j] - prices[i];
+			if (profit > maxProfit) {
+				maxProfit = profit;
+			}
+		}
+	}
+	cout << maxProfit;
+}
