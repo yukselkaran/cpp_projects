6a5e48e9fee91e14c3c7c13e379f459149f17990
Yüksel Karan <ykselkaran53@gmail.com>
2025-07-11 16:39:09 +0300

    DefangingIpAddress

---
+++ DefangingIpAddress.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+//defang tehlikeli olabilecek bir şey zararsız hale getirmek anlamında kullanılıyor, burada gerçek anlamı da dişlerini kesmek.
+string DefangingIpAddress(string address){
+	string result = "";
+	for (char c : address) {
+		if (c == '.') {
+			result += "[.]";
+		}
+		else {
+			result += c;
+		}
+	}
+	return result;
+}
+int main()
+{
+	string addres1 = "192.168.1.1";
+	cout << DefangingIpAddress(addres1);
+}
