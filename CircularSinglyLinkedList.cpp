d2aa6addf24b34e54f81656ea421b41fa6c99df6
Yüksel Karan <ykselkaran53@gmail.com>
2025-11-21 02:29:32 +0300

    Add files via upload

---
+++ CircularSinglyLinkedList.cpp
@@ -0,0 +1,53 @@
+﻿#include <iostream>
+using namespace std;
+
+class Node {
+public:
+	int data;
+	Node* next;
+	Node(int d) {
+		data = d;
+		next = nullptr;
+	}
+};
+
+class CircularLinkedList {
+public:
+	Node* head;
+	CircularLinkedList() {
+		head = nullptr;
+	}
+
+	void insert(int value) {
+		Node* newNode = new Node(value);
+		if (head == nullptr) {
+			head = newNode;
+			head->next = head; // head connect itself if list has 0 item before insertion.
+			return;
+		}
+		Node* current = head;
+		while (current->next != head) {// if it is before the head we will add it right after the last node before head.
+			current = current->next;
+		}
+		current->next = newNode;
+		newNode->next = head; // this line also makes the circular structure.
+	}
+
+	void printList() {
+		if (head == nullptr) {
+			cout << "List is empty";
+			return;
+		}
+		Node* current = head;
+		do {// the reason why we use do-while loop at here is. We initialized current as equals to head and it makes the condition that inside the loop becomes false.
+			cout << current->data << " -> ";
+			current = current->next;
+		} while (current != head);
+		cout << "(Head)" << endl;
+	}
+};
+
+int main() {
+
+	return 0;
+}
